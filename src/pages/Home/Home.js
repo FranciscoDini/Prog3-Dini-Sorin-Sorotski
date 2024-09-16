103bad148a3c90c88c7b64078fc49df686a548f1
AgosSorotski <asorotski@udesa.edu.ar>
2024-09-16 16:08:17 -0300

    cambios en home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,5 @@
 import {Component} from "react";
-import HomeMovies from "../HomeMovies/HomeMovies"
+import HomeMovies from "../../components/HomeMovies/HomeMovies"
 
 class Home extends Component{
     constructor(props){
