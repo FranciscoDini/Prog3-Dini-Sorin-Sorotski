cc34d6c18f15c0000c87277ca982b3e709f482e7
francisco_dini <21franciscodini@gmail.com>
2024-09-19 23:01:11 -0300

    ver mas pt2

--- src/components/HomeMovies/HomeMovies.js
+++ src/components/HomeMovies/HomeMovies.js
@@ -1,44 +1,53 @@
 import { Component } from "react";
-import Movie from "../Movie/Movie"
-import "./HomeMovies.css"
+import Movie from "../Movie/Movie";
+import "./HomeMovies.css";
 
-
-class HomeMovies extends Component{
-    
-constructor(props){
+class HomeMovies extends Component {
+  constructor(props) {
     super(props);
-    this.state={
-       peliculas:[]
-    }
+    this.state = {
+      peliculas: [],
+      peliculasMostradas: 5,
     };
-    componentDidMount() {
-        const { url } = this.props;
-        fetch(url)
-          .then((response) => response.json())
-          .then((data) => {
-            this.setState({ peliculas: data.results.slice(0, 5) });
-          })
-          .catch((error) => {
-            console.log("El error fue:", error);
-          });
-      }
-    
-    render(){
-        const {peliculas}= this.state; 
-        
-        return (
-            <div className="Home-movies">
-                {peliculas.map((pelicula,idx)=>(
-                    <Movie
-                    key={idx}
-                    pelicula={pelicula}
-                />
-                ))}
-            </div>
-        );
-    }
+  }
+
+  componentDidMount() {
+    const { url } = this.props;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ peliculas: data.results });
+      })
+      .catch((error) => {
+        console.log("El error fue:", error);
+      });
+  }
+
+  cargarMasPeliculas = () => {
+    this.setState((mostradas) => ({
+      peliculasMostradas: mostradas.peliculasMostradas + 5,
+    }));
+  };
+
+  render() {
+    const { peliculas, peliculasMostradas } = this.state;
+    const { mostrarVerMas } = this.props;
+
+    return (
+      <div>
+        <div className="Home-movies">
+          {peliculas.slice(0, peliculasMostradas).map((pelicula, idx) => (
+            <Movie key={idx} pelicula={pelicula} />
+          ))}
+        </div>
+        {mostrarVerMas && peliculasMostradas < peliculas.length && (
+          <div className="showMore">
+            <button onClick={this.cargarMasPeliculas}>Ver más</button>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
-export default HomeMovies
-
-
+export default HomeMovies;
--- src/pages/Populares.js
+++ src/pages/Populares.js
@@ -1,9 +1,24 @@
-const Populares = () => {
+import { Component } from "react";
+import HomeMovies from "../components/HomeMovies/HomeMovies";
+//import {Link} from "react-router-dom"
+
+class Populares extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      peliculasPopulares: [],
+    };
+  }
+
+  render() {
+    const popularesUrl = "https://api.themoviedb.org/3/movie/popular?api_key=1d1ffcbd926e19d7721125f17a8319dc";
     return (
-      <>
-        
-      </>
+      <div className="populares">
+        <h1>Películas populares</h1>
+        <HomeMovies url={popularesUrl} mostrarVerMas={true} /> 
+      </div>
     );
-  };
-  
-  export default Populares
+  }
+}
+
+export default Populares;
