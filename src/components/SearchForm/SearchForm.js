9b7abec1f71ae07c6af503b055a8c530d2848ab2
francisco_dini <21franciscodini@gmail.com>
2024-09-20 21:03:14 -0300

    inicio search

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,42 @@
+import { Component } from "react";
+
+class SearchForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      query: "",
+    };
+  }
+
+  handleFormChange(e) {
+    this.setState({
+      query: e.target.value,
+    });
+  }
+
+  handleCancelSubmit(e) {
+    e.preventDefault();
+  }
+
+  handleFormSubmit(){
+    this.props.history.push('/search', {query:this.state.query})
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={(e) => this.handleCancelSubmit}>
+          <input
+            onChange={(e) => this.handleFormChange(e)}
+            name="query"
+            value={this.state.query}
+          />
+          <button onClick={()=>this.handleFormSubmit()}>Search</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchForm;
--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,9 +1,14 @@
+import React from 'react'
 import "./pages.css"
+import { PiDropSimple } from 'react-icons/pi';
 
-const Search = () => {
+const Search = (props) => {
+
+  <PiDropSimple/>
+
     return (
       <>
-        
+      <div>{props.location.state.query}</div>
       </>
     );
   };
